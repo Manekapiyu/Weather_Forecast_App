e8bc67f366a62eeedc6d957197298a792d17768e
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-11-22 17:51:38 +0530

    development

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from tkinter import *
 import tkinter as tk
 from geopy.geocoders import Nominatim
@@ -20,6 +21,32 @@ Round_box=PhotoImage(file="Images/Rounded Rectangle 1.png")
 Label (root,image=Round_box="#57adff").place(x=30,y=110)
 
 #label
-label1=Label(root,text="Temprature",font=('Helvetiica'))
+label1=Label(root,text="Temprature",font=('Helvetiica',11), fg="white" , bg="#203243")
+label1.place(x=50,y=120)
+
+label2=Label(root,text="Humidity",font=('Helvetiica',11), fg="white" , bg="#203243")
+label2.place(x=50,y=120)
+
+label3=Label(root,text="Pressure",font=('Helvetiica',11), fg="white" , bg="#203243")
+label3.place(x=50,y=120)
+
+label4=Label(root,text="Wind Speed",font=('Helvetiica',11), fg="white" , bg="#203243")
+label4.place(x=50,y=120)
+
+label5=Label(root,text="Description",font=('Helvetiica',11), fg="white" , bg="#203243")
+label5.place(x=50,y=120)
+
+##search box
+Sreach_image=PhotoImage(file="Images/Rounded Rectangle 3.png")
+myimage=Label(image=Sreach_image,bg="#57adff")
+myimage.place(x=270,y=120)
+
+weat_image=PhotoImage(file="Images/Layer 7.png")
+weatherimage=Label(root,image=weat_image,bg="#203243")
+weatherimage.place(x=290,y=127)
+
+textfield=tk.entry(root,justify='center',width=15,font=('poppins',25,'bold'),bg="#203243",border=0 , fg="white")
+textfield.place(x=370)
+
 
 root.mainloop()
